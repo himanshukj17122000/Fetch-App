6950fee4c60a2ffe2d0bf4f4ed85e51121555f0c
bt96 <bt96@duke.edu>
2020-04-13 01:46:25 -0400

    updated the user page

---
+++ JCoding/ProfileTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTableViewController.swift
+//  JCoding
+//
+//  Created by Bilge Tatar on 4/12/20.
+//  Copyright © 2020 himanshu. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
