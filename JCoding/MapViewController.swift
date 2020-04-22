5898fc865d4414f1c4c49d85a53a0dd39909deee
Donald Groh <dkg16@duke.edu>
2020-04-21 20:33:02 -0400

    finished the map

--- JCoding/MapViewController.swift
+++ JCoding/MapViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate{
 
     @IBOutlet weak var backBtn: UIButton!
     @IBOutlet weak var mapView: MKMapView!
@@ -19,6 +19,8 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        mapView.showsUserLocation = true
         addAnnotation()
 
         // Do any additional setup after loading the view.
@@ -30,6 +32,15 @@ class MapViewController: UIViewController {
             let location = CLLocation(latitude: Double(user.dogslat), longitude: Double(user.dogslong))
             
             let annotation = UserAnnotation()
+            annotation.title = user.dogname
+            annotation.subtitle = "Gender: \(user.dogGender)"
+//            let profileImageurl = user.profileImageUrl
+            let url = URL(string:user.profileImageUrl)
+            if let data = try? Data(contentsOf: url!)
+            {
+                let image: UIImage = UIImage(data: data)!
+                annotation.profileImage = image
+            }
             annotation.coordinate = location.coordinate
             nearByAnnotations.append(annotation)
             
@@ -53,7 +64,51 @@ class MapViewController: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
-
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyPin"
+        var annotationView: MKAnnotationView?
+        
+        // reuse the annotation if possible
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "icon-user")
+        } else if let deqAno = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) {
+            annotationView = deqAno
+            annotationView?.annotation = annotation
+        } else {
+            let annoView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annoView.rightCalloutAccessoryView = UIButton(type: UIButton.ButtonType.detailDisclosure)
+            annotationView = annoView
+        }
+        
+        if let annotationView = annotationView, let anno = annotation as? UserAnnotation {
+            annotationView.canShowCallout = true
+            
+            let image = anno.profileImage
+            let resizeRenderImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            resizeRenderImageView.layer.cornerRadius = 25
+            resizeRenderImageView.clipsToBounds = true
+            resizeRenderImageView.contentMode = .scaleAspectFill
+            resizeRenderImageView.image = image
+            
+            UIGraphicsBeginImageContextWithOptions(resizeRenderImageView.frame.size, false, 0.0)
+            resizeRenderImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+            let thumbnail = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            annotationView.image = thumbnail
+            
+//            let btn = UIButton()
+//            btn.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+//            btn.setImage(UIImage(named: "icon-direction"), for: UIControl.State.normal)
+//            annotationView.rightCalloutAccessoryView = btn
+//
+//            let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+//            annotationView.leftCalloutAccessoryView = leftIconView
+        }
+        return annotationView
+    }
     /*
     // MARK: - Navigation
 
